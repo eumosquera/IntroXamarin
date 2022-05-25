aef1d0c99c081c1d3c81b0131ca59e6568c3bd44
eumosquera <endermu28@gmail.com>
2022-05-24 22:48:10 -0500

    Add project files.

---
+++ IntroXamarin.App/IntroXamarin.App/Views/Forms/Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntroXamarin.App.Views.Forms
+{
+    internal class Calculator
+    {
+        public static double Calculate(double value1, double value2, string Operator)
+        {
+            double result = 0;
+
+            switch (Operator)
+            {
+                case "÷":
+                    result = value1 / value2;
+                    break;
+
+                case "x":
+                    result = value1 * value2;
+                    break;
+
+                case "+":
+                    result = value1 + value2;
+                    break;
+
+                case "-":
+                    result = value1 - value2;
+                    break;
+
+                case "%":
+                    result = (value1 / 100) * value2;
+                    break;
+
+            }
+
+            return result;
+
+        }
+
+    }
+}
